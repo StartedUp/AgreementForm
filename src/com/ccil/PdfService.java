5dbbe1cdc6d344f411b7c9192f15537097ba807a
PrithuGit <prithviprakashr@gmail.com>
2016-04-18 23:00:12 +0530

    Ajax tested pdf saved

--- src/com/ccil/PdfService.java
+++ src/com/ccil/PdfService.java
@@ -6,7 +6,9 @@ import java.util.List;
 import java.util.Map;
 
 import javax.ws.rs.DefaultValue;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
@@ -28,12 +30,13 @@ public class PdfService {
 	@GET
 	@Path("/editForm")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response runPython(@DefaultValue("") @QueryParam("data") String data,@DefaultValue("") @QueryParam("agreementType") String agreementType) {
-		data = "{\"firstName\":\"Prithvi\", \"lastName\": \"Prakash\"}";
+	public Response runPython(@DefaultValue("") @QueryParam("data") String data) {
+		//data = "{\"firstName\":\"Prithvi\", \"lastName\": \"Prakash\"}";
 		Gson gson = new Gson();
 		Type type = new TypeToken<Map<String, String>>() {
 		}.getType();
 		Map<String, String> fieldNameAndValue = gson.fromJson(data, type);
+		String agreementType=fieldNameAndValue.get("agreementType");
 		System.out.println(fieldNameAndValue);
 		String src = "G:/my_db/MyGit/StartedUp/AgreementForm/"+agreementType+"Src.pdf";
 		String test = "G:/my_db/MyGit/StartedUp/AgreementForm/"+agreementType+".pdf";
@@ -46,7 +49,7 @@ public class PdfService {
 			List<PDField> fields = acroForm.getFields();
 			for (PDField pdField : fields) {
 				System.out.println(
-						pdField.getFullyQualifiedName() + " " + fieldNameAndValue.get(pdField.getFullyQualifiedName()));
+						pdField.getFullyQualifiedName());
 				if (pdField != null) {
 					pdField.setValue(fieldNameAndValue.get(pdField.getFullyQualifiedName()));
 					pdField.setReadOnly(true);
